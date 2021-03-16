f911ea6bc6d4fc75257ed3f0527f87a9e2120a45
Oliver 'kfsone' Smith <oliver@kfs.org>
2021-03-16 13:26:20 -0700

    Removed testify dependency
    Sample (manually induced) failure of errormsg_test.go:
    
    ```
    (base) oliver@Spud:/mnt/c/Users/oliver/go/src/github.com/kfsone/gocc/example/errormsg$ go test .
    --- FAIL: TestErrors_Error (0.00s)
        --- FAIL: TestErrors_Error/custom_error (0.00s)
            errormsg_test.go:139: mismatch: expected 6:9: error: source on fire, got 6:7: error: source on fire
    FAIL
    FAIL    github.com/goccmack/gocc/example/errormsg       0.002s
    FAIL
    ```

--- example/errormsg/errormsg_test.go
+++ example/errormsg/errormsg_test.go
@@ -4,29 +4,35 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/stretchr/testify/require"
-
 	"github.com/goccmack/gocc/example/errormsg/errors"
 	"github.com/goccmack/gocc/example/errormsg/lexer"
 	"github.com/goccmack/gocc/example/errormsg/parser"
 	"github.com/goccmack/gocc/example/errormsg/token"
-
-	"github.com/stretchr/testify/assert"
 )
+
+func assertEqual(t *testing.T, lhs interface{}, rhs interface{}) {
+	t.Helper()
+	if lhs != rhs {
+		t.Fatalf("mismatch: expected %v, got %v", lhs, rhs)
+	}
+}
 
 func parse(t *testing.T, code string, expectPass bool) {
 	t.Helper()
 
 	l := lexer.NewLexer([]byte(code))
 	p := parser.NewParser()
-	sym, err := p.Parse(l)
+	_, err := p.Parse(l)
 
-	if expectPass {
-		if assert.Nil(t, err) {
-			assert.NotNil(t, sym)
+	// Match err to expectPass (nil = passed, !nil = failed), and log if not nill
+	if err == nil {
+		if !expectPass {
+			t.Fatal("test should have failed, got nil error instead")
 		}
-	} else if assert.Error(t, err) {
-		assert.Nil(t, sym)
+	} else {
+		if expectPass && err != nil {
+			t.Fatalf("test should have passed, got error instead: %s", err.Error())
+		}
 		t.Log(err.Error())
 	}
 }
@@ -87,20 +93,20 @@ func mockToken(tokenType token.Type, lit string, line, col int) *token.Token {
 
 func TestErrors_DescribeExpected(t *testing.T) {
 	t.Run("none", func(t *testing.T) {
-		assert.Equal(t, "unexpected additional tokens", errors.DescribeExpected([]string{}))
+		assertEqual(t, "unexpected additional tokens", errors.DescribeExpected([]string{}))
 	})
 	t.Run("single", func(t *testing.T) {
-		assert.Equal(t, "expected TREE", errors.DescribeExpected([]string{"TREE"}))
+		assertEqual(t, "expected TREE", errors.DescribeExpected([]string{"TREE"}))
 	})
 	t.Run("either", func(t *testing.T) {
-		assert.Equal(t, "expected either TREE or ENT", errors.DescribeExpected([]string{"TREE", "ENT"}))
+		assertEqual(t, "expected either TREE or ENT", errors.DescribeExpected([]string{"TREE", "ENT"}))
 	})
 	t.Run("oxford comma", func(t *testing.T) {
 		t.Run("list of 3", func(t *testing.T) {
-			assert.Equal(t, "expected one of TREE, ENT or i am groot", errors.DescribeExpected([]string{"TREE", "ENT", "i am groot"}))
+			assertEqual(t, "expected one of TREE, ENT or i am groot", errors.DescribeExpected([]string{"TREE", "ENT", "i am groot"}))
 		})
 		t.Run("longer list", func(t *testing.T) {
-			assert.Equal(t, "expected one of a, b, c, d, e, f, or g", errors.DescribeExpected([]string{"a", "b", "c", "d", "e", "f", "g"}))
+			assertEqual(t, "expected one of a, b, c, d, e, f, or g", errors.DescribeExpected([]string{"a", "b", "c", "d", "e", "f", "g"}))
 		})
 	})
 }
@@ -108,43 +114,46 @@ func TestErrors_DescribeExpected(t *testing.T) {
 func TestErrors_DescribeToken(t *testing.T) {
 	t.Run("eof", func(t *testing.T) {
 		tok := mockToken(token.EOF, "-not-eof-", 1, 1)
-		assert.Equal(t, errors.EOFRepresentation, errors.DescribeToken(tok))
+		assertEqual(t, errors.EOFRepresentation, errors.DescribeToken(tok))
 	})
 	t.Run("eof", func(t *testing.T) {
 		tok := mockToken(token.INVALID, "-not-eof-", 1, 1)
-		assert.Equal(t, "unknown/invalid token \"-not-eof-\"", errors.DescribeToken(tok))
+		assertEqual(t, "unknown/invalid token \"-not-eof-\"", errors.DescribeToken(tok))
 	})
 	t.Run("eof", func(t *testing.T) {
 		tok := mockToken(9001, "-not-eof-", 1, 1)
-		assert.Equal(t, "\"-not-eof-\"", errors.DescribeToken(tok))
+		assertEqual(t, "\"-not-eof-\"", errors.DescribeToken(tok))
 	})
 }
 
 // More direct testing by manually constructing Error objects.
 func TestErrors_Error(t *testing.T) {
 	// anticipated messages are based on this assumption.
-	require.Equal(t, "error", errors.Severity)
+	assertEqual(t, "error", errors.Severity)
 
 	t.Run("custom error", func(t *testing.T) {
 		err := &errors.Error{ErrorToken: mockToken(999, "", 6, 7), Err: fmt.Errorf("source on fire")}
-		if assert.Error(t, err) {
-			assert.Equal(t, "6:7: error: source on fire", err.Error())
+		if err == nil {
+			t.Fatalf("failed to produce an error")
 		}
+		assertEqual(t, "6:7: error: source on fire", err.Error())
 	})
 
 	t.Run("no tokens", func(t *testing.T) {
 		err := &errors.Error{ErrorToken: mockToken(888, "biscuit", 10, 12), ExpectedTokens: []string{}}
-		assert.Equal(t, "10:12: error: unexpected additional tokens; got: \"biscuit\"", err.Error())
+		assertEqual(t, "10:12: error: unexpected additional tokens; got: \"biscuit\"", err.Error())
 	})
 
 	t.Run("unexpected EOF", func(t *testing.T) {
-		require.Equal(t, "<EOF>", errors.EOFRepresentation)
+		if errors.EOFRepresentation != "<EOF>" {
+			panic("EOFRepresentation has changed")
+		}
 		err := &errors.Error{ErrorToken: mockToken(token.EOF, "", 7, 11), ExpectedTokens: []string{"something-else"}}
-		assert.Equal(t, "7:11: error: expected something-else; got: <EOF>", err.Error())
+		assertEqual(t, "7:11: error: expected something-else; got: <EOF>", err.Error())
 	})
 
 	t.Run("nominal error", func(t *testing.T) {
 		err := &errors.Error{ErrorToken: mockToken(100, "42", 7, 6), ExpectedTokens: []string{"var", "let", "struct"}}
-		assert.Equal(t, "7:6: error: expected one of var, let or struct; got: \"42\"", err.Error())
+		assertEqual(t, "7:6: error: expected one of var, let or struct; got: \"42\"", err.Error())
 	})
 }
